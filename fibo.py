9befcb01a4a82c9e9254b0fc597c964d78ddc613
Marek Kollár <marek@wd02-0230.wd02-0230a.kn.vutbr.cz>
2024-04-22 12:06:41 +0200

    1

--- fibo.py
+++ fibo.py
@@ -1,7 +1,15 @@
+def fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibo(n - 1) + fibo(n - 2)
 
 def main():
     pass
 
 
 if __name__ == "__main__":
+    print(fibo(10))
     main()
